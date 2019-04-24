dc8eb8b21a6046ad18a874732e2b2a1edc6ff3bd
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-24 19:55:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,8 @@ class School
 
 
   def add_student(name,grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    @roster.include?(grade)? @roster[grade] << name : @roster[grade] = [] @roster[grade] << name
+    
   end
 
 end
