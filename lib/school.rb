667d41dd1009ad6b35daddacf5d74416361137c7
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-24 19:38:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,11 +3,12 @@
 class School
   def initialize(name)
     @name = name
-  end
-
-  def roster
     @roster = {}
   end
+
+  # def roster
+  #   @roster = {}
+  # end
 
   def add_student(name,grade)
     @roster[grade] = []
